f36f6b2149530f477db191398e67eb9bcb9858ed
colt005 <rohan.prabhu05@gmail.com>
2022-06-19 11:53:24 +0530

    Add main file with BulkSave logic

--- main.go
+++ main.go
@@ -1,29 +1,49 @@
-package main
+package BulkIt
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/gabriel-vasile/mimetype"
+	"github.com/google/uuid"
+	logger "github.com/sirupsen/logrus"
+	"github.com/zenthangplus/goccm"
 )
 
-type ImageSave struct {
-	client *HTTPClient
+type BulkIt struct {
+	client     *HTTPClient
+	maxThreads int
 }
 
 type HTTPClient struct {
 	client *http.Client
 }
 
-func NewImageSave(c *http.Client) *ImageSave {
+/// Create an FileSave object which takes in an http Client to make requests and an integer value which controls the number of threads to be fired.
+/// If `c` is nil, default http.Client will be assigned.
+/// If `maxThreads` is 0 then `maxThreads` will default to 30
+func NewBulkIt(c *http.Client, maxThreads int) *BulkIt {
 	if c == nil {
 		c = &http.Client{}
+	}
+
+	if maxThreads == 0 {
+		maxThreads = 30
 	}
 
 	h := &HTTPClient{
 		client: c,
 	}
 
-	return &ImageSave{
-		client: h,
+	return &BulkIt{
+		client:     h,
+		maxThreads: maxThreads,
 	}
 }
 
@@ -44,10 +64,89 @@ func (c *HTTPClient) GET(url string) (*http.Response, error) {
 	return resp, nil
 }
 
-func TestRunFunc() {
-	fmt.Println("Running")
+func (im *BulkIt) SaveFilesByUrls(urls []string, path string) {
+	startTime := time.Now()
+
+	c := goccm.New(im.maxThreads)
+	counter := 0
+	for i := 0; i < len(urls); i++ {
+		c.Wait()
+		go func(i int64) {
+			err := worker(*im.client, urls[i], path)
+			if err != nil {
+				logger.Error("Failed to download url : ", urls[i])
+			}
+			if err == nil {
+				counter += 1
+			}
+			fmt.Printf("Downloaded %d of %d \n", i+1, len(urls))
+			c.Done()
+		}(int64(i))
+	}
+	c.WaitAllDone()
+	logger.Info("%d of %d files downloaded successfully", counter, len(urls))
+	logger.Info("Time Taken : " + time.Since(startTime).String())
 }
 
-func main() {
-	TestRunFunc()
+func worker(client HTTPClient, url string, fPath string) (err error) {
+	if url == "" {
+		return
+	}
+	// fmt.Println("Downloading..." + url)
+	resp, err := client.GET(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		resp.Body.Close()
+		return
+	}
+	defer resp.Body.Close()
+	// fmt.Println(resp.Header)
+
+	if resp.StatusCode != http.StatusOK {
+		return errors.New(`failed to download. Please check url`)
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// detectedFileType := http.DetectContentType(bodyBytes)
+	detectedFileType := mimetype.Detect(bodyBytes).String()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fName := uuid.New().String()
+	// TODO : Use Content-Disposition header for fileName if available
+	fileName := fName + getFileExtension(detectedFileType)
+	tmpPath := filepath.Join(fPath, fileName)
+	if getFileExtension(detectedFileType) == "" {
+		logger.Error("Failed to get file extension")
+		return
+	}
+	newFile, err := os.Create(tmpPath)
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	defer newFile.Close()
+
+	if _, err = newFile.Write(bodyBytes); err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	return
+}
+
+func getFileExtension(mimeType string) (extension string) {
+	ext, err := mime.ExtensionsByType(mimeType)
+	if err != nil {
+		return
+	}
+	return ext[len(ext)-1]
 }
